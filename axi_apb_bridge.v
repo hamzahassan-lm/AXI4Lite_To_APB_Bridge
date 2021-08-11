284e1cd82cea537b9418445ca2e83353f66ccca9
aghaalizeb-lm <agha.ali@lampromellon.com>
2021-08-11 17:51:17 +0500

    fixed comparison bits

--- axi_apb_bridge.v
+++ axi_apb_bridge.v
@@ -168,21 +168,21 @@ assign SWDATA        = reg_m_apb_pwdata;
 // 	endcase
 // end
 
-assign sel_m_apb_prdata = (m_apb_psel == 16'h0001) & m_apb_prdata   |
-                          (m_apb_psel == 16'h0002) & m_apb_prdata2  |
-                          (m_apb_psel == 16'h0004) & m_apb_prdata3  |
-                          (m_apb_psel == 16'h0008) & m_apb_prdata4  |
-                          (m_apb_psel == 16'h0010) & m_apb_prdata5  |
-                          (m_apb_psel == 16'h0020) & m_apb_prdata6  |
-                          (m_apb_psel == 16'h0040) & m_apb_prdata7  |
-                          (m_apb_psel == 16'h0080) & m_apb_prdata8  |
-                          (m_apb_psel == 16'h0100) & m_apb_prdata9  |
-                          (m_apb_psel == 16'h0200) & m_apb_prdata10 |
-                          (m_apb_psel == 16'h0400) & m_apb_prdata11 |
-                          (m_apb_psel == 16'h0800) & m_apb_prdata12 |
-                          (m_apb_psel == 16'h1000) & m_apb_prdata13 |
-                          (m_apb_psel == 16'h2000) & m_apb_prdata14 |
-                          (m_apb_psel == 16'h4000) & m_apb_prdata15 |
-                          (m_apb_psel == 16'h8000) & m_apb_prdata16;
+assign sel_m_apb_prdata = {32{(m_apb_psel == 16'h0001)}} & m_apb_prdata   |
+                          {32{(m_apb_psel == 16'h0002)}} & m_apb_prdata2  |
+                          {32{(m_apb_psel == 16'h0004)}} & m_apb_prdata3  |
+                          {32{(m_apb_psel == 16'h0008)}} & m_apb_prdata4  |
+                          {32{(m_apb_psel == 16'h0010)}} & m_apb_prdata5  |
+                          {32{(m_apb_psel == 16'h0020)}} & m_apb_prdata6  |
+                          {32{(m_apb_psel == 16'h0040)}} & m_apb_prdata7  |
+                          {32{(m_apb_psel == 16'h0080)}} & m_apb_prdata8  |
+                          {32{(m_apb_psel == 16'h0100)}} & m_apb_prdata9  |
+                          {32{(m_apb_psel == 16'h0200)}} & m_apb_prdata10 |
+                          {32{(m_apb_psel == 16'h0400)}} & m_apb_prdata11 |
+                          {32{(m_apb_psel == 16'h0800)}} & m_apb_prdata12 |
+                          {32{(m_apb_psel == 16'h1000)}} & m_apb_prdata13 |
+                          {32{(m_apb_psel == 16'h2000)}} & m_apb_prdata14 |
+                          {32{(m_apb_psel == 16'h4000)}} & m_apb_prdata15 |
+                          {32{(m_apb_psel == 16'h8000)}} & m_apb_prdata16;
 
 endmodule
